6854eb81fa2d52f0282a2a0e935f7f2aa73f62e6
RishikeshSV <rishuvidyarthi@gmail.com>
2021-07-23 18:08:32 +0530

    banner background image inserted

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import requests from "../Assets/requests";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
+const baseImgUrl = "https://image.tmdb.org/t/p/original";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    const data = async () => {
+      const request = await api.get(requests.fetchNetflixOriginals);
+      let randMovieIndex = Math.floor(
+        Math.random() * request.data.results.length - 1
+      );
+      setMovie(request.data.results[randMovieIndex]);
+    };
+    data();
+  }, []);
   return (
     <>
-      <h1>THIS IS THE BANNER</h1>
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("${baseImgUrl}${movie?.backdrop_path}")`,
+        }}
+      >
+        <div className="content">
+          <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        </div>
+      </header>
     </>
   );
 }
